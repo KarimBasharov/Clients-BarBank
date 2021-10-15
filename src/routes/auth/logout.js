c196b3077f95d9f1c6b6f3e7efeee88cc8e09cb8
opilane <karimbasharov@gmail.com>
2021-10-15 14:04:09 +0300

    As a user I see an overview page when I log on [Delivers #179948344]

--- src/routes/auth/logout.js
+++ src/routes/auth/logout.js
@@ -1,8 +1,8 @@
 import * as api from 'api.js';
 
-export function post(req, res) {
+export async function post(req, res) {
     api.del('sessions', req.session.token).then(response => {
         delete req.session.token;
         res.end(JSON.stringify(response));
-    });
+    })
 }
